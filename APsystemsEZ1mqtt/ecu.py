f2beff22110bf4335a1d21080475453405bafaf8
Holger Müller <github@euhm.de>
2024-06-01 10:25:23 +0200

    extended from class APsystemsEZ1M, no need to hold the inverter instance and removed update_data() and update_info() methods

--- APsystemsEZ1mqtt/ecu.py
+++ APsystemsEZ1mqtt/ecu.py
@@ -4,7 +4,7 @@
 """Handle APsystemsEZ1M ECU requests"""
 import logging
 
-from APsystemsEZ1 import APsystemsEZ1M, ReturnDeviceInfo, ReturnOutputData
+from APsystemsEZ1 import APsystemsEZ1M
 from astral import LocationInfo
 from astral.sun import daylight
 from datetime import datetime, timedelta
@@ -12,9 +12,9 @@ from datetime import datetime, timedelta
 _LOGGER = logging.getLogger(__name__)
 
 
-class ECU:
-    def __init__(self, ecu_config):
-        self.inverter = APsystemsEZ1M(ecu_config.ipaddr, ecu_config.port)
+class ECU(APsystemsEZ1M):
+    def __init__(self, ecu_config, timeout: int = 10):
+        super().__init__(ecu_config.ipaddr, ecu_config.port, timeout)
         self.stop_at_night = ecu_config.stop_at_night
         if self.stop_at_night:
             self.city = LocationInfo("", "", 
@@ -41,31 +41,3 @@ class ECU:
     def wake_up_time(self):
         night_start, night_end = self.night()
         return night_end
-
-
-    async def update_data(self) -> ReturnOutputData | None:
-        _LOGGER.debug("Start ECU update data")
-        data = None
-
-        try:
-            # Get output data
-            data = await self.inverter.get_output_data()
-            _LOGGER.debug(f"Output Data: {data}")
-        except Exception as e:
-            # Handle any exceptions that occur during the data fetch and print the error.
-            _LOGGER.warning("An exception occured: %s -> %s", e.__class__.__name__, str(e))
-        return data
-
-
-    async def update_info(self) -> ReturnDeviceInfo | None:
-        _LOGGER.debug("Start ECU update info")
-        info = None
-
-        try:
-            # Get output data
-            info = await self.inverter.get_device_info()
-            _LOGGER.debug(f"Output Info: {info}")
-        except Exception as e:
-            # Handle any exceptions that occur during the data fetch and print the error.
-            _LOGGER.warning("An exception occured: %s -> %s", e.__class__.__name__, str(e))
-        return info
