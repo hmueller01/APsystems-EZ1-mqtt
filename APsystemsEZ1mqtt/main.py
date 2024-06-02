d8883101e2abe378166cbdec03651b95d78ddbbb
Holger Müller <github@euhm.de>
2024-06-02 20:28:33 +0200

    added setting power status and max power using async callbacks
    fixed get_device_info() exception if EZ1 does not respond

--- APsystemsEZ1mqtt/main.py
+++ APsystemsEZ1mqtt/main.py
@@ -5,7 +5,6 @@
 import asyncio
 import logging
 import sys
-import time
 
 from APsystemsEZ1 import ReturnDeviceInfo
 from APsystemsEZ1mqtt.config import Config
@@ -15,7 +14,8 @@ from argparse import ArgumentParser
 from datetime import datetime, timedelta
 
 _ecu: ECU
-_LOGGER = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
+_loop: asyncio.AbstractEventLoop
 _mqtt: MQTTHandler
 
 
@@ -31,38 +31,101 @@ def cli_args():
 
 async def async_on_status_power(status: bool):
     """Async callback function for MQTTHandler on_status_power"""
-    global _ecu, _mqtt
-    _LOGGER.debug(f"Start async_on_status_power({status})")
+    global _ecu, _logger, _mqtt
+    _logger.debug(f"Start async_on_status_power(status={status})")
     status_power = await _ecu.set_status_power(status)
     _mqtt.publish_status_power(status_power)
 
 
+async def async_on_max_power(value: int):
+    """Async callback function for MQTTHandler on_max_power"""
+    global _ecu, _logger, _mqtt
+    _logger.debug(f"Start async_on_max_power(value={value})")
+    max_power = await _ecu.set_max_power(value)
+    _mqtt.publish_max_power(max_power)
+
+
+async def periodic_wakeup():
+    """Periodic wakeup is needed to let new tasks from other threads be executed"""
+    while True:
+        await asyncio.sleep(1)
+
+
+async def periodic_get_data(interval: float):
+    while True:
+        now = datetime.now()
+        _logger.debug(f"Start periodic_get_data: {now}")
+        if _ecu.is_night(now):
+            sleeptime = _ecu.wake_up_time().timestamp() - now.timestamp()
+        else:
+            sleeptime = interval
+            try:
+                ecu_data = await _ecu.get_output_data()
+                _mqtt.publish_data(ecu_data)
+            except Exception as e:
+                _logger.error("An exception occured: %s -> %s", e.__class__.__name__, str(e))
+
+        next_update_time = (now.astimezone(_ecu.city.tzinfo) + timedelta(0, sleeptime)).strftime("%Y-%m-%d %H:%M:%S %Z")
+        # compensate code runtime
+        sleeptime = max(0, sleeptime - (datetime.now().timestamp() - now.timestamp()))
+        _logger.debug("Next update at: %s (in %0.2fs)", next_update_time, sleeptime)
+        await asyncio.sleep(sleeptime)
+
+
+async def periodic_get_power(interval: float):
+    while True:
+        now = datetime.now()
+        _logger.debug(f"Start periodic_get_power: {now}")
+        if _ecu.is_night(now):
+            sleeptime = _ecu.wake_up_time().timestamp() - now.timestamp() + interval
+        else:
+            sleeptime = interval
+            try:
+                max_power = await _ecu.get_max_power()
+                _mqtt.publish_max_power(max_power)
+                status_power = await _ecu.get_status_power()
+                _mqtt.publish_status_power(status_power)
+            except Exception as e:
+                _logger.error("An exception occured: %s -> %s", e.__class__.__name__, str(e))
+
+        next_update_time = (now.astimezone(_ecu.city.tzinfo) + timedelta(0, sleeptime)).strftime("%Y-%m-%d %H:%M:%S %Z")
+        _logger.debug("Next update at: %s (in %0.2fs)", next_update_time, sleeptime)
+        await asyncio.sleep(sleeptime)
+
+
 async def main():
     """Main application. Does not return. Terminate using <Ctrl>-C."""
-    global _ecu, _LOGGER, _mqtt
+    global _ecu, _logger, _mqtt, _loop
+    
+    _loop = asyncio.get_event_loop()
     args = cli_args()
     conf = Config(args.config_path)
     if not conf.ecu_config.ipaddr:
-        _LOGGER.error("APS_ECU_IP not found. No config given? Use -h")
+        _logger.error("APS_ECU_IP not found. No config given? Use -h")
         sys.exit(1)
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
                         format="%(levelname)s:%(name)s.%(funcName)s(): %(message)s")
     _ecu = ECU(conf.ecu_config)
 
-    while (ecu_info := await _ecu.get_device_info()) is None:
-        if args.debug:
-            _LOGGER.info("Can't read APsystems info data. Setting dummy data.")
-            ecu_info = ReturnDeviceInfo(
-                deviceId='debug dummy Id',
-                devVer='debug dummy Ver',
-                ssid='debug dummy ssid',
-                ipAddr='192.168.9.9',
-                minPower=int(30),
-                maxPower=int(800))
-            break
-        else:
-            _LOGGER.error("Can't read APsystems info data. Waiting for a minute ...")
-            time.sleep(60)
+    _logger.info("Read data from APsystems EZ1 at http://%s:%d", conf.ecu_config.ipaddr, conf.ecu_config.port)
+    ecu_info = None
+    while ecu_info is None:
+        try:
+            ecu_info = await _ecu.get_device_info()
+        except Exception as e:
+            pass
+            if args.debug:
+                _logger.info("Can't read APsystems info data. Setting dummy data.")
+                ecu_info = ReturnDeviceInfo(
+                    deviceId='123456789',
+                    devVer='debug dummy Ver',
+                    ssid='debug dummy ssid',
+                    ipAddr='192.168.9.9',
+                    minPower=int(30),
+                    maxPower=int(800))
+            else:
+                _logger.error("Can't read APsystems info data. Waiting for a minute ...")
+                await asyncio.sleep(60)
 
     if not conf.mqtt_config.homa_systemid:
         # if no homa_systemid is given in config use deviceId
@@ -70,7 +133,9 @@ async def main():
     if not conf.mqtt_config.hass_device_id:
         # if no hass_device_id is given in config use deviceId
         conf.mqtt_config.hass_device_id = ecu_info.deviceId
-    _mqtt = MQTTHandler(asyncio.get_event_loop(), async_on_status_power, conf.mqtt_config, retain = not args.debug)
+    _mqtt = MQTTHandler(lambda status: _loop.call_soon_threadsafe(asyncio.create_task, async_on_status_power(status)),
+                        lambda value: _loop.call_soon_threadsafe(asyncio.create_task, async_on_max_power(value)),
+                        conf.mqtt_config, retain = not args.debug)
     _mqtt.connect_mqtt()
     
     # if -r is passed remove all retained topics and exit
@@ -81,34 +146,14 @@ async def main():
     _mqtt.hass_init(conf.ecu_config, ecu_info) # must init before homa_init
     _mqtt.homa_init(ecu_info, _ecu.city.tzinfo)
 
-    # do this only once a day
-    max_power = await _ecu.get_max_power()
-    status_power = await _ecu.get_status_power()
-    _mqtt.publish_max_power(max_power)
-    _mqtt.publish_status_power(status_power)
-
-    while True:
-        now = datetime.now()
-        if _ecu.is_night(now):
-            sleeptime = _ecu.wake_up_time().timestamp() - now.timestamp()
-        else:
-            sleeptime = float(conf.ecu_config.update_interval)
-            try:
-                ecu_data = await _ecu.get_output_data()
-                if ecu_data is not None:
-                    _mqtt.publish_data(ecu_data)
-            except Exception as e:
-                _LOGGER.error("An exception occured: %s -> %s", e.__class__.__name__, str(e))
-
-        _LOGGER.info("Next update at: %s", (now.astimezone(_ecu.city.tzinfo) + timedelta(0, sleeptime)).strftime("%Y-%m-%d %H:%M:%S %Z"))
-        # compensate code runtime
-        sleeptime = max(0, sleeptime - (datetime.now().timestamp() - now.timestamp()))
-        _LOGGER.debug(f"Time to sleep: {sleeptime:0.2f}s")
-        time.sleep(sleeptime)
-        #await asyncio.sleep(sleeptime)
+    _logger.info("Started all periodic tasks. Press <Ctrl>-C to terminate.")
+    await asyncio.gather(
+        periodic_wakeup(),
+        periodic_get_data(conf.ecu_config.update_interval),
+        periodic_get_power(600), # 10min update interval
+    )
 
 
 if __name__ == "__main__":
-    # Run the main coroutine.
-    # This is the entry point of the script and it runs the main function in an asynchronous manner.
+    # This is the entry point of the script and it runs the main function in an asynchronous manner (coroutine).
     asyncio.run(main())
